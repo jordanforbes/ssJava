b0d95ca6dd51392a9c3d60ef367a36268a834941
jordanforbes <jordforb@gmail.com>
2020-12-14 18:32:04 -0500

    finished assessment

---
+++ src/com/ss/week1assessmentDateTime/DateTimeTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.ss.week1assessmentDateTime;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author Jordf
+ *
+ */
+public class DateTimeTest {
+	
+	public static void main(String[] args) {
+	
+	//1. LocalDateTime
+
+	//2	
+		
+	final LocalDate someDate = randomDate();
+	
+	System.out.println(lastThursday(someDate));
+	}
+	
+	//finds the previous thursday to a LocalDate object
+	public static LocalDate lastThursday(LocalDate date) {
+		LocalDate lastInMonth = date.with(TemporalAdjusters.lastInMonth(DayOfWeek.THURSDAY));
+		return lastInMonth;
+		
+	}
+	
+	//creates random date to plug into the last lastThursday()
+	public static LocalDate randomDate() {
+		//100 years
+		return LocalDate.ofEpochDay(ThreadLocalRandom.current().nextInt(-3600,3600));
+		
+	}
+	
+}
